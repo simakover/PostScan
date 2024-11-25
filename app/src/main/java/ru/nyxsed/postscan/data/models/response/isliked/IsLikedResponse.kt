f45fb0ea31854cea107a17ca0ebba0eeb54ee304
nyxsed <nyxsed@mail.ru>
2024-11-25 22:51:34 +0500

    content update via pager

---
+++ app/src/main/java/ru/nyxsed/postscan/data/models/response/isliked/IsLikedResponse.kt
@@ -0,0 +1,9 @@
+package ru.nyxsed.postscan.data.models.response.isliked
+
+
+import com.google.gson.annotations.SerializedName
+
+data class IsLikedResponse(
+    @SerializedName("response")
+    val response: IsLikedContentResponse
+)
