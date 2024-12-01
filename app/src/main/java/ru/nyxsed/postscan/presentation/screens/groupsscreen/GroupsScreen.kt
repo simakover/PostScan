a9347a81fb633a8145f870a6073455c924e43921
nyxsed <nyxsed@mail.ru>
2024-12-02 02:05:36 +0500

    deleting posts with group

--- app/src/main/java/ru/nyxsed/postscan/presentation/screens/groupsscreen/GroupsScreen.kt
+++ app/src/main/java/ru/nyxsed/postscan/presentation/screens/groupsscreen/GroupsScreen.kt
@@ -16,10 +16,8 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -28,18 +26,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.composegears.tiamat.navController
 import com.composegears.tiamat.navDestination
-import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 import ru.nyxsed.postscan.R
+import ru.nyxsed.postscan.data.models.entity.GroupEntity
 import ru.nyxsed.postscan.presentation.screens.addgroupscreen.AddGroupScreen
 import ru.nyxsed.postscan.presentation.screens.pickgroupscreen.PickGroupScreen
 
@@ -49,16 +45,16 @@ val GroupsScreen by navDestination<Unit> {
 
     val navController = navController()
     val snackbarHostState = SnackbarHostState()
-    val scope = rememberCoroutineScope()
-    val context = LocalContext.current
 
-    var showDialog by remember { mutableStateOf(false) }
+    var showAddDialog by remember { mutableStateOf(false) }
+    var showDeleteDialog by remember { mutableStateOf(false) }
+    lateinit var groupToDelete: GroupEntity
 
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    showDialog = true
+                    showAddDialog = true
                 }
             ) {
                 Icon(
@@ -79,7 +75,7 @@ val GroupsScreen by navDestination<Unit> {
         ) {
             items(
                 items = groupsState.value,
-                key = { it.groupId!! }
+                key = { it.groupId }
             ) {
                 Box(
                     modifier = Modifier
@@ -88,20 +84,8 @@ val GroupsScreen by navDestination<Unit> {
                     GroupCard(
                         group = it,
                         onGroupDeleteClicked = {
-                            groupScreenViewModel.deleteGroup(it)
-                            scope.launch {
-                                snackbarHostState.currentSnackbarData?.dismiss()
-                                val snackbarResult = snackbarHostState.showSnackbar(
-                                    message = context.getString(R.string.group_deleted),
-                                    actionLabel = context.getString(R.string.undo),
-                                    duration = SnackbarDuration.Short
-                                )
-                                if (snackbarResult == SnackbarResult.ActionPerformed) {
-                                    groupScreenViewModel.addGroup(it)
-                                } else {
-                                    groupScreenViewModel.deleteAllPostsForGroup(it)
-                                }
-                            }
+                            groupToDelete = it
+                            showDeleteDialog = true
                         },
                         onGroupClicked = {
                             navController.navigate(AddGroupScreen, it)
@@ -111,25 +95,89 @@ val GroupsScreen by navDestination<Unit> {
                 }
             }
         }
-        ShowModalDialog(
-            showDialog = showDialog,
+        ShowAddModalDialog(
+            showDialog = showAddDialog,
             onDismiss = {
-                showDialog = false
+                showAddDialog = false
             },
             onManuallyAddClicked = {
-                showDialog = false
+                showAddDialog = false
                 navController.navigate(AddGroupScreen)
             },
             onPickClicked = {
-                showDialog = false
+                showAddDialog = false
                 navController.navigate(PickGroupScreen)
+            }
+        )
+        ShowDeleteModalDialog(
+            showDialog = showDeleteDialog,
+            onDismiss = {
+                showDeleteDialog = false
+            },
+            onConfirmClicked = {
+                showDeleteDialog = false
+                groupScreenViewModel.deleteGroup(groupToDelete)
+                groupScreenViewModel.deleteAllPostsForGroup(groupToDelete)
             }
         )
     }
 }
 
 @Composable
-fun ShowModalDialog(
+fun ShowDeleteModalDialog(
+    showDialog: Boolean,
+    onDismiss: () -> Unit,
+    onConfirmClicked: () -> Unit,
+) {
+    if (showDialog) {
+        Dialog(
+            onDismissRequest = onDismiss
+        ) {
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = MaterialTheme.colorScheme.surface
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .height(200.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.delete_group),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        modifier = Modifier
+                            .weight(1f),
+                        text = stringResource(R.string.group_delete_dialog_question)
+                    )
+                    TextButton(
+                        onClick = {
+                            onConfirmClicked()
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(R.string.confirm)
+                        )
+                    }
+                    TextButton(
+                        onClick = {
+                            onDismiss()
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(R.string.cancel)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowAddModalDialog(
     showDialog: Boolean,
     onDismiss: () -> Unit,
     onManuallyAddClicked: () -> Unit,
